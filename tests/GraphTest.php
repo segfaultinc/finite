e9ae00b6971af582612ff957d95a1bf5223bf6c6
jerguslejko <jergus.lejko@gmail.com>
2019-02-16 12:28:45 +0000

    split tests

--- tests/GraphTest.php
+++ tests/GraphTest.php
@@ -6,8 +6,6 @@ use SegfaultInc\Finite\Graph;
 use SegfaultInc\Finite\State;
 use PHPUnit\Framework\TestCase;
 use SegfaultInc\Finite\Transition;
-use SegfaultInc\Finite\Exceptions\InvalidStateException;
-use SegfaultInc\Finite\Exceptions\NoInitialStateException;
 
 class GraphTest extends TestCase
 {
@@ -30,48 +28,6 @@ class GraphTest extends TestCase
     }
 
     /** @test */
-    public function it_finds_state_by_key()
-    {
-        $finite = (new Graph)
-            ->setStates([
-                $init = State::initial('init'),
-            ]);
-
-        $this->assertSame($init, $finite->getStates()->find('init'));
-    }
-
-    /** @test */
-    public function it_throws_when_trying_to_find_non_existing_state()
-    {
-        $this->expectException(InvalidStateException::class);
-
-        $finite = (new Graph);
-
-        $finite->getStates()->find('NON-EXISTING');
-    }
-
-    /** @test */
-    public function it_finds_initial_state()
-    {
-        $finite = (new Graph)
-            ->setStates([
-                $init = State::initial('init'),
-            ]);
-
-        $this->assertSame($init, $finite->getStates()->initial());
-    }
-
-    /** @test */
-    public function it_throws_when_trying_to_find_non_existing_initial_state()
-    {
-        $this->expectException(NoInitialStateException::class);
-
-        $finite = (new Graph);
-
-        $finite->getStates()->initial();
-    }
-
-    /** @test */
     public function can_register_transitions()
     {
         $machine = (new Graph)
@@ -86,7 +42,10 @@ class GraphTest extends TestCase
             ]);
 
         $this->assertCount(2, $machine->getTransitions());
-        $this->assertSame($one, $machine->getTransitions()->toArray()[0]);
-        $this->assertSame($two, $machine->getTransitions()->toArray()[1]);
+
+        $this->assertSame(
+            [$one, $two],
+            $machine->getTransitions()->toArray()
+        );
     }
 }
